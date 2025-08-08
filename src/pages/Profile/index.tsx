9d3d09e90a939a9977e8fe5a0e6005c6c0dc042a
Nguyen1976 <nguyen2202794@gmail.com>
2025-08-08 08:47:54 +0700

    feat: enhance Profile page layout with user details and bio section

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import { faArrowLeft, faLock, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faArrowLeft, faCalendarDays, faLock, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useParams } from 'react-router-dom'
 
@@ -23,7 +23,48 @@ export default function Profile() {
         </div>
       </div>
       {/* Profile */}
-      <div className='h-[1000px] text-2xl text-white'> profile</div>
+      <div className='border-[1px] dark:border-zinc-800'>
+        {/* header-img */}
+        <img src='https://pbs.twimg.com/profile_banners/1541985824622796800/1754536449/1080x360' alt='' />
+        {/* Edit profile */}
+        <div className='relative flex justify-end py-3 px-6'>
+          <div className='h-36 w-36 rounded-full overflow-hidden absolute -top-[72px] left-6 border-4 border-black'>
+            <img src='https://pbs.twimg.com/profile_images/1541985856071667713/9VYgARp-_400x400.png' alt='' />
+          </div>
+          <button className='rounded-full px-4 py-1 border-[1px] border-zinc-500 dark:text-white dark:bg-black font-medium'>
+            Edit profile
+          </button>
+        </div>
+
+        {/* ... */}
+        <div className='mt-5 px-5'>
+          <div className='items-start flex flex-col dark:text-white'>
+            <div className='flex items-center gap-4'>
+              <p className='font-bold text-xl'>Nguyen Nguyen</p>
+              <FontAwesomeIcon icon={faLock} />
+            </div>
+            <p className='text-sm text-zinc-600'>@shouta9271</p>
+            {/* bio */}
+            <div className='mt-2'>React Developer | Dream in code, live in pixels.</div>
+            {/* created-at */}
+            <div className='text-zinc-600 flex items-center gap-2 mt-3'>
+              <FontAwesomeIcon icon={faCalendarDays} />
+              <p>Joined June 2022</p>
+            </div>
+            {/* Flow */}
+            <div className='flex items-center gap-5 mt-2'>
+              <div className='flex items-center gap-2'>
+                <p>8</p>
+                <p className='text-zinc-600'>Following</p>
+              </div>
+              <div className='flex items-center gap-2'>
+                <p>2</p>
+                <p className='text-zinc-600'>Followers</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* Tabs phần này sẽ xử lý sau khi bên backend làm xong*/}
     </>
   )
